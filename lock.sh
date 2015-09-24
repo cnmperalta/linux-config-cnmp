6c6b6d120c8ecd55be9b4d44cb002c6a85c82345
Kei Peralta <cnmperalta@gmail.com>
2015-09-24 10:44:15 +0800

    Add script and conf files

---
+++ lock.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+i3lock -i Pictures/Wallpapers/oneos_dark_1920x1080.png -c 000000 -p win -n
---
+++ proxy.sh
@@ -0,0 +1,5 @@
+export http_proxy=http://202.92.144.96:3129/
+export https_proxy=$http_proxy
+export ftp_proxy=$http_proxy
+export rsync_proxy=$http_proxy
+export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
